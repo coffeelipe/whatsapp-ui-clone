95990f675e69c125729b8382b234ee6c296d469d
Felipe Pontes <fpontes321@gmail.com>
2026-03-11 20:29:12 -0300

    Implement basic web layout

--- whatsapp_ui/lib/pages/web_screen_layout.dart
+++ whatsapp_ui/lib/pages/web_screen_layout.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/widgets/contacts_list.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('WEB')));
+    return Scaffold(
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  // app bar
+                  // search bar
+                  ContactsList(),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.75,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/backgroundImage.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
